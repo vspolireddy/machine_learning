e5defb14c1ec4216f5128cda7f812b2be1a53843
vspolireddy <vspolireddy@gmail.com>
2020-04-20 02:18:57 -0400

    adding notes and cleaning up

--- 02_Regression/simple_linear_regression.py
+++ 02_Regression/simple_linear_regression.py
@@ -1,3 +1,14 @@
+# =============================================================================
+# Regression models (both linear and non-linear) are used for 
+# predicting a real value, like salary for example. If your
+# independent variable is time, then you are forecasting 
+# future values, otherwise your model is predicting present but 
+# unknown values. Regression technique vary from Linear Regression to 
+# SVR and Random Forests Regression.
+# =============================================================================
+
+
+
 # Simple linear Regression
 #y =b + b * x
 #    0   1   1
@@ -7,7 +18,7 @@
 # b0 = constant
 
 #best fit of line = Sum (yi - yi^)^2 -> min
-
+#ordinary leasts squares methods
 
 # Importing the libraries
 import numpy as np
@@ -20,41 +31,38 @@ X = dataset.iloc[:, :-1].values
 y = dataset.iloc[:, 1].values
 
 # Splitting the dataset into the Training set and Test set
-from sklearn.cross_validation import train_test_split
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 1/3, random_state = 0)
-
-# Feature Scaling
-# from sklearn.preprocessing import StandardScaler
-# sc_X = StandardScaler()
-# X_train = sc_X.fit_transform(X_train)
-# X_test = sc_X.transform(X_test)
-# sc_y = StandardScaler()
-# y_train = sc_y.fit_transform(y_train)
+from sklearn.model_selection import train_test_split
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state = 0)
 
 
-
-#fitting simple linear regression to the traning set
+#training
+# regression is prediction of continous variable
 from sklearn.linear_model import LinearRegression
 regressor = LinearRegression()
-regressor.fit(X_train,y_train)
+regressor.fit(X_train, y_train)
 
 
-#predict the Test set results
+
+#predicting the test set results
 y_pred = regressor.predict(X_test)
 
 
-# Visualising the Training set results
+#visualing the traning set results
 plt.scatter(X_train, y_train, color = 'red')
-plt.plot(X_train, regressor.predict(X_train), color = 'blue')
-plt.title('Salary vs Experience (Training set)')
-plt.xlabel('Years of Experience')
-plt.ylabel('Salary')
+plt.plot(X_train, regressor.predict(X_train), color ='blue')
+plt.title('Salary v. Experience (Traning set)')
+plt.xlabel('Years of experience')
+plt.ylabel('salary')
 plt.show()
 
-# Visualising the Test set results
-# plt.scatter(X_test, y_test, color = 'red')
-# plt.plot(X_train, regressor.predict(X_train), color = 'blue')
-# plt.title('Salary vs Experience (Test set)')
-# plt.xlabel('Years of Experience')
-# plt.ylabel('Salary')
-# plt.show()
+
+#visualing the test set results
+plt.scatter(X_test, y_test, color = 'red')
+plt.plot(X_train, regressor.predict(X_train), color ='blue')
+plt.title('Salary v. Experience (test set)')
+plt.xlabel('Years of experience')
+plt.ylabel('salary')
+plt.show()
+
+
+
